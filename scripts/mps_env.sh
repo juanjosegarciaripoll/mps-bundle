9ba6cf5523d44f5ebba9b6fbe4e29b4c725fc598
Juanjo Garcia-Ripoll <jjgarcia@users.sourcerforge.net>
2013-08-23 12:14:37 +0200

    Change build options in CeSViMa

--- scripts/mps_env.sh
+++ scripts/mps_env.sh
@@ -1,17 +1,14 @@
 case `hostname -f` in
     login*mad) # CeSViMa
 	# Atlas library
-	if [ "x$1" = "xtensor" ]; then
-	    # export CPPFLAGS=-I/sw/openmpi/ATLAS/3.8.4-gnu64-4.3/include
-	    # export LDFLAGS="-L/sw/openmpi/ATLAS/3.8.4-gnu64-4.3/lib"
-	    # export LIBS="-llapack -lcblas -lf77blas -latlas -lgfortran"
-	    # export CC=gcc-4.3
-	    # export CXX=g++-4.3
-	    echo > /dev/null
-	fi
+	export CPPFLAGS=-I/sw/openmpi/ATLAS/3.8.4-gnu64-4.3/include
+	export LDFLAGS="-L/sw/openmpi/ATLAS/3.8.4-gnu64-4.3/lib"
+	export LIBS="-llapack -lcblas -lf77blas -latlas -lgfortran"
+	export CC=gcc-4.3
+	export CXX=g++-4.3
 	# ESSL library
-	export CC="xlc -q64"
-	export CXX="xlC -q64"
+	#export CC="xlc -q64"
+	#export CXX="xlC -q64"
 	;;
     master*csic.es) # trueno.iff.csic.es
 	if [ "x$1" = "xtensor" ]; then
