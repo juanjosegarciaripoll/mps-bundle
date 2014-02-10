b7eb3579c1cd47f4818b57ea01a526808b1c3f96
Juanjo Garcia-Ripoll <jjgarcia@users.sourcerforge.net>
2014-02-10 10:56:46 +0100

    Upgrade compiler flags for Trueno.

--- scripts/mps_env.sh
+++ scripts/mps_env.sh
@@ -15,8 +15,7 @@ case `hostname -f` in
 	export CC=icc
 	export CXX=icpc
 	export CFLAGS="$CFLAGS -fomit-frame-pointer -finline-functions"
-	export CXXFLAGS="$CXXFLAGS 
-	-fno-exceptions -fomit-frame-pointer -finline-functions"
+	export CXXFLAGS="$CXXFLAGS -fno-exceptions -fomit-frame-pointer -finline-functions"
 	#export CFLAGS="$CFLAGS -xHost -fomit-frame-pointer -finline-functions"
 	#export CXXFLAGS="$CXXFLAGS -xHost -fno-exceptions -fomit-frame-pointer -finline-functions"
 	;;
